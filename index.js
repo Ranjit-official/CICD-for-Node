085f143bbc55453a5eb12d7459bd3ad5f55762f6
clever-king-402 <122508802+clever-king-402@users.noreply.github.com>
2025-05-01 22:04:58 +0545

    seventh try

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const IP = '127.0.0.1'
 
 const app = express()
 app.get("/",(req,res,next)=>{
-    res.send("this is my sixth try")
+    res.send("this is my seventh try")
 })
 
 const server = http.createServer(app)
